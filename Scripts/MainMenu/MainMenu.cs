d3f9232fa7bd0b26845413344faab2b49ffb110d
Berkay DEMİR <bberkaydemir@hotmail.com>
2024-03-12 22:49:04 +0300

    Main Menu Script
    Script Setup

---
+++ Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PlayGame()
+    {
+        get
+    }
+
+}
